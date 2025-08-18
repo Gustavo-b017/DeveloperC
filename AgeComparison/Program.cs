eea7c9d5997a0f844c4852f55c1fc32b39369290
Laboratório FIAP <labsfiap@fiap.com.br>
2025-08-18 11:54:46 -0300

    aula 2 tarefa

---
+++ AgeComparison/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age1, age2;
+
+            Console.WriteLine("Insira a idade do aluno 1");
+            age1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Insira a idade do aluno 2");
+            age2 = int.Parse(Console.ReadLine());
+
+            if (age1 == age2)
+            {
+                Console.WriteLine("O Aluno 1 tem as mesma idade do aluno 2");
+            }
+            if (age1 > age2)
+            {
+                Console.WriteLine("O Aluno 1 é mais velho que o Aluno 2");
+            }
+            if (age1 < age2)
+            {
+                Console.WriteLine("O Aluno 1 é mais novo que o Aluno 2");
+            }
+
+
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,2 @@
+## Para rodar o projeto caso tenha dado o acesso negado:
+* Add-MpPreference -AttackSurfaceReductionOnlyExclusions "(caminho completo da pasta)"
