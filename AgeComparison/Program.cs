14916348df35f773d656584045934e76deec2c0c
labsfiap <labsfiap@PA106MICRO10>
2025-08-25 10:53:05 -0300

    primeira sobrecarga de metodo e criando funcoes

--- AgeComparison/Program.cs
+++ AgeComparison/Program.cs
@@ -4,30 +4,34 @@ namespace MyApp
 {
     internal class Program
     {
+        static double raio = 0;
+
+        static void Print(string message)
+        {
+            Console.WriteLine(message);
+        }
+        static void Print(double message)
+        {
+            Console.WriteLine(message);
+        }
+
+        static double CalcularCirculo(double valor)
+        {
+            double raio = 0;
+            raio = Math.Pow(valor, 2) * Math.PI;
+
+            return raio;
+        }
+
         static void Main(string[] args)
         {
-            int age1, age2;
+            Print("Qual é o raio do circulo?");
 
-            Console.WriteLine("Insira a idade do aluno 1");
-            age1 = int.Parse(Console.ReadLine());
+            raio = double.Parse(Console.ReadLine()); // le o valor do raio escrita no terminal e coloca em uma variavel do tipo double
 
-            Console.WriteLine("Insira a idade do aluno 2");
-            age2 = int.Parse(Console.ReadLine());
-
-            if (age1 == age2)
-            {
-                Console.WriteLine("O Aluno 1 tem as mesma idade do aluno 2");
-            }
-            if (age1 > age2)
-            {
-                Console.WriteLine("O Aluno 1 é mais velho que o Aluno 2");
-            }
-            if (age1 < age2)
-            {
-                Console.WriteLine("O Aluno 1 é mais novo que o Aluno 2");
-            }
-
-
+            double resultado = CalcularCirculo(raio); // o resultado é do tipo double
+            Print("area do circulo é: " + resultado); // porcausa da concatenação o resultado é convertido para string
+            Print(resultado); // por causa da sobrecarga o resultado é impresso como double
         }
     }
 }
