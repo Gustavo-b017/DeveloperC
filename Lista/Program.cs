f46c704db0121c5c084182da84325b837ae9ddb4
Gustavo <gustavo.estudo017@gmail.com>
2025-09-15 11:24:44 -0300

    senhas

---
+++ Lista/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //array de int
+            int[] listaNumeros = { 5, 6, 4, 13 };
+
+            int[] numeros = { 0, 0, 0, 0 };
+
+            while (true)
+            {
+                Console.WriteLine("Tente descobrir a senha ****");
+
+                for (int i = 0; i < listaNumeros.Length; i++)
+                {
+                    Console.WriteLine("Digito " + (i + 1));
+                    numeros[i] = int.Parse(Console.ReadLine());
+
+                    if (numeros[i] == listaNumeros[i])
+                    {
+                        Console.WriteLine("Digito correto");
+                        if (i == listaNumeros.Length - 1)
+                        {
+                            Console.WriteLine("Senha correta");
+                            return;
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Digito incorreto");
+                        break;
+                    }
+                }
+
+            }
+
+            Console.WriteLine("Terminando");
+
+
+        }
+    }
+}
---
+++ Senhas/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Array de números
+            int[] listaNumeros = { 5, 6, 4, 13 };
+
+            Console.WriteLine("Digite 4 números separados por espaço:");
+            string[] entrada = Console.ReadLine().Split(' ');
+            int[] numeros = Array.ConvertAll(entrada, int.Parse);
+
+            // Verifica se o usuário digitou 4 números
+            if (entrada.Length != listaNumeros.Length)
+            {
+                Console.WriteLine("Você deve digitar exatamente 4 números.");
+                return;
+            }
+
+            for (int i = 0; i < listaNumeros.Length; i++)
+            {
+                if (numeros[i] == listaNumeros[i])
+                {
+                    Console.WriteLine($"Dígito {i + 1} correto");
+                    if (i == listaNumeros.Length - 1)
+                    {
+                        Console.WriteLine("Senha correta");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"Dígito {i + 1} incorreto");
+                }
+            }
+
+        }
+    }
+}
