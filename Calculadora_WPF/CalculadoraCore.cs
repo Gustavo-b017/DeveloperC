e1200cae2faf86f5b8371cbec7db5a0b9a33b469
Gustavo <gustavo.estudo017@gmail.com>
2025-10-13 18:21:39 -0300

    WPF

---
+++ Calculadora_WPF/CalculadoraCore.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora_WPF
+{
+    internal class CalculadoraCore
+    {
+        double[] doubles;
+        int index;
+
+        public CalculadoraCore()
+        {
+            doubles = new double[8];
+            index = 0;
+        }
+
+        /// <summary>
+        /// Função que adiciona um numero ao buffer
+        /// </summary>
+        /// <param name="valor">valor que será armazenado</param>
+        /// <returns>retorna false se buffer cheio</returns>
+        public bool AdicionarBuffer(double valor)
+        {
+            if(index >= doubles.Length)
+            {
+                return false;
+            }
+
+            doubles[index] = valor;
+            index++;
+            return true;
+        }
+
+        public double Processor()
+        {
+            double sum = 0;
+            foreach (double d in doubles)
+            {
+                sum += d;
+            }
+            return sum;
+        }
+    }
+}
---
+++ Calculadora_WPF/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Calculadora_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        CalculadoraCore core = new CalculadoraCore();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void textoSaida_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            double valor = core.Processor();
+
+            testeSaida.Text = valor.ToString();
+        }
+
+
+        private void CampoEntrada_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void BtnBuffer_Click_1(object sender, RoutedEventArgs e)
+        {
+            double valor;
+
+            if (double.TryParse(CampoEntrada.Text, out valor))
+            {
+
+
+                if (core.AdicionarBuffer(valor))
+                {
+                    conteudo.Text += valor.ToString() + "\n";
+                    CampoEntrada.Text = "";
+                }
+            }
+        }
+
+        private void clear_Click(object sender, RoutedEventArgs e)
+        {
+            core = new CalculadoraCore();
+            CampoEntrada.Text = string.Empty;
+            conteudo.Text = string.Empty;
+            testeSaida.Text = string.Empty;
+        }
+    }
+}
